7e3f2dda69953d1be3474204348b452b6ab7e821
simon mwaura <161325035+simonmwaura@users.noreply.github.com>
2024-05-09 17:13:14 +0300

    Add files via upload

---
+++ installment instruction.js
@@ -0,0 +1,20 @@
+/*
+Do project using vite
+vite link=https://vitejs.dev/guide/
+
+1)scroll till you find Scaffolding Your First Vite Project
+copy the npm command 
+command=npm create vite@latest
+
+2)put it in terminal
+type to add your project name when prompted
+
+3)use arrow keys
+to select for instance im using react and javascript
+
+4)run the next code commands 
+the first one is to enter the file we made
+the 2nd is npm install to install node modules
+npm run dev to go live
+
+ */
---
+++ setuptailwind css.js
@@ -0,0 +1,20 @@
+/*
+set up tailwind using vite
+1)open the website 
+link=https://tailwindcss.com/
+2)open  docs
+2)open installation then go to framework guides
+3)open vite
+4)ignore step 1 beause we already have an application
+5)copy the 1st command under Install Tailwind CSS
+the command=npm install -D tailwindcss postcss autoprefixer
+6)after its downloaded run the 2nd command this will create tailwind.config.js
+command=npx tailwindcss init -p
+7)step 3 Configure your template paths
+swap the content in tailwind tab with the content in tailwind.config.js
+just copy paste
+8)follow step 4 Add the Tailwind directives to your CSS
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+*/
